cfe6d1d143d469695e4d507323c325bf24adea56
Wolf Alex <al.volf.av@gmail.com>
2018-02-11 17:09:04 +0300

    upload

---
+++ src/Frago9876543210/WebServer/Connection.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frago9876543210\WebServer;
+
+
+use raklib\utils\InternetAddress;
+
+class Connection{
+	/** @var resource $socket */
+	protected $socket;
+
+	/**
+	 * Connection constructor.
+	 * @param resource $socket
+	 */
+	public function __construct($socket){
+		$this->socket = $socket;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function read() : string{
+		return socket_read($this->socket, 1024);
+	}
+
+	/**
+	 * @param string $data
+	 */
+	public function write(string $data) : void{
+		socket_write($this->socket, $data);
+	}
+
+	/**
+	 * Closes the connection
+	 */
+	public function close() : void{
+		socket_close($this->socket);
+	}
+
+	/**
+	 * @param string $type
+	 * @param string $data
+	 */
+	public function send(string $type, string $data){
+		$this->write("HTTP/1.1 200 OK\r\nContent-Type: " . $type . "\r\n\r\n" . $data);
+	}
+
+	/**
+	 * @return null|InternetAddress
+	 */
+	public function getAddress() : ?InternetAddress{
+		return socket_getpeername($this->socket, $address, $port) ? new InternetAddress($address, $port, 4) : null;
+	}
+}
---
+++ src/Frago9876543210/WebServer/WebServer.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frago9876543210\WebServer;
+
+
+use pocketmine\Thread;
+use raklib\utils\InternetAddress;
+
+class WebServer extends Thread{
+	/** @var resource $socket */
+	protected $socket;
+	/** @var callable $handler */
+	protected $handler;
+	/** @var InternetAddress $bindAddress */
+	protected $bindAddress;
+	/** @var bool $isRunning */
+	protected $isRunning = true;
+
+	/**
+	 * WebServer constructor.
+	 * @param InternetAddress $bindAddress
+	 * @param callable        $handler
+	 * @throws \Exception
+	 */
+	public function __construct(InternetAddress $bindAddress, callable $handler){
+		$this->bindAddress = $bindAddress;
+		$this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		if(@!socket_bind($this->socket, $bindAddress->getIp(), $bindAddress->getPort())){
+			throw new \Exception("Failed to bind to " . $bindAddress->__toString());
+		}
+		socket_set_option($this->socket, SOL_SOCKET, SO_REUSEADDR, 1);
+		socket_listen($this->socket);
+		$this->handler = $handler;
+	}
+
+	public function run() : void{
+		$this->registerClassLoader();
+
+		while($this->isRunning){
+			if(is_resource(($client = socket_accept($this->socket)))){
+				$connection = new Connection($client);
+				call_user_func($this->handler, $connection, $connection->read());
+			}
+		}
+	}
+
+	/**
+	 * @return InternetAddress
+	 */
+	public function getBindAddress() : InternetAddress{
+		return $this->bindAddress;
+	}
+
+	/**
+	 * Disables socket processing
+	 */
+	public function shutdown() : void{
+		$this->isRunning = false;
+	}
+}
